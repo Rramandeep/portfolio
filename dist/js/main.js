7e68db8793f6afc2038e44f577afe6fc4d867029
Raman <Rraman063@gmail.com>
2018-07-29 00:20:27 +0530

    menu button js & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// select DOM items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".branding");
+const navItem = document.querySelectorAll(".nav-item");
+
+//set initial state
+let showMenu = false;
+
+menuBtn.addEventListener("click", togglemenu);
+
+function togglemenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItem.forEach(item => item.classList.add("show"));
+
+    //set show menu true
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItem.forEach(item => item.classList.remove("show"));
+
+    //set menu state
+    showMenu = false;
+  }
+}
